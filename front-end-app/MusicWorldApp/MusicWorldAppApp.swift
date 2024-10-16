a05bd704a935ce06d3571ea1ab927cf755d343ee
DylanLi-Hang <hang20145@gmail.com>
2024-10-16 14:07:00 +1100

    Initial commit app dev

---
+++ front-end-app/MusicWorldApp/MusicWorldAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  MusicWorldAppApp.swift
+//  MusicWorldApp
+//
+//  Created by Dylan on 16/10/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MusicWorldAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
